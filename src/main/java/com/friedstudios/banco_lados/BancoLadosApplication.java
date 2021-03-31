87a6c301ae8cdf834b2bb356fad349aeb716ac34
Pelli <martinpelli@outlook.com>
2021-03-30 21:10:52 -0300

    initial commit

---
+++ src/main/java/com/friedstudios/banco_lados/BancoLadosApplication.java
@@ -0,0 +1,13 @@
+package com.friedstudios.banco_lados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoLadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoLadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/friedstudios/banco_lados/BancoLadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friedstudios.banco_lados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoLadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
