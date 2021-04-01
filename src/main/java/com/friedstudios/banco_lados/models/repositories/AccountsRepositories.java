852a7b68e9bc6f526fd29e92b341df52eb5a14b7
Pelli <friedstudios@outlook.com>
2021-03-31 23:39:19 -0300

    Add Accounts Controller

--- src/main/java/com/friedstudios/banco_lados/models/repositories/AccountsRepositories.java
+++ src/main/java/com/friedstudios/banco_lados/models/repositories/AccountsRepositories.java
@@ -3,5 +3,8 @@ package com.friedstudios.banco_lados.models.repositories;
 import com.friedstudios.banco_lados.models.entities.AccountEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AccountsRepositories extends CrudRepository<AccountEntity,Long> {
+    List<AccountEntity> findAllByUserId(String userId);
 }
